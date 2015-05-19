2b7ca6c5f02a5c58632342f678a8b0a26b86f9e1
leroi <leroidejesa@gmail.com>
2015-05-19 14:22:36 -0700

    update monster methods

--- lib/monster.rb
+++ lib/monster.rb
@@ -6,26 +6,26 @@ class Monster < ActiveRecord::Base
     where({:killed_by_player => false})
   end)
 
+  private
+
+  def attack(player)
+    if self.room_id == player.room_id
+      player_hp = 0.+(player.hp)
+      player_hp -= @ap
+      puts 'Player screams, "AAawwgghghghh!!11!1!"'
+  end
+
 
   private
 
-  attr_reader(:hp, :ap)
-
-  def initialize(attributes)
-    @hp = attributes[:hp]
-    @ap = attributes[:ap]
-    @killed_by_player = attributes[:killed_by_player]
-  end
-
-  def attack(player)
-    player_hp = 0.+(player.hp)
-    player_hp -= @ap
-    player_hp
-  end
-
-  def drop_loot
-    if self.alive? == false
+  def dies
+    if :hp <= 0
+      self.update({:killed_by_player == true})
       dropped_item = Item.find(rand 10)
+      dropped_item.update(:room_id => self.room_id())
+    end
+    puts ''
   end
+
 
 end
