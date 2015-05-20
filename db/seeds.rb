77b7d4ae133d012861f77ff678fc1b83b961c722
leroi <leroidejesa@gmail.com>
2015-05-20 12:20:52 -0700

    add more items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,28 @@ require('./lib/monster.rb')
 #keep in mind: will be at least 10 levels if we each come up with at least 2-3
 
 # items dropped from monsters (need one per monster)
-Item.create(:name => "Epicodus T-Shirt, Size: XL", :attack_damage=>0, :in_backpack? => false, :room_id => nil)
-Item.create(:name =>"Nunchucks", :attack_damage=>7, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"A Baby Seal", :attack_damage=>5, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Machete", :attack_damage=>9, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Rusty Fork", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name => "Epicodus T-Shirt, Size: XL", :attack_damage=>0, :hp_modifier=>1, :in_backpack? => false, :room_id => nil)
+Item.create(:name =>"Nunchucks", :attack_damage=>7, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"11 Tins of SPAM", :attack_damage=>0, :hp_modifier=>20, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Machete", :attack_damage=>9, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Miller High Life, 32oz", :attack_damage=>0, :hp_modifier=>50, :in_backpack?=>false, :room_id=>nil)
 
 # standard items found in rooms
-Item.create(:name =>"Katana", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Stapler (brand new)", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Apple", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"12oz Can of 'Mountain Dew'", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Sandwich (rotten)", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Whiskey, double, on the rocks", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Fire Extinguisher", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Nerf Gun", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
-Item.create(:name =>"Fork", :attack_damage=>2, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Katana", :attack_damage=>2, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Stapler (brand new)", :attack_damage=>2, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Apple", :attack_damage=>2, :hp_modifier=>5, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"12oz Can of 'Mountain Dew'", :attack_damage=>2, :hp_modifier=>5, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Sandwich (rotten)", :attack_damage=>2, :hp_modifier=>3, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Whiskey, double, on the rocks", :attack_damage=>2, :hp_modifier=>100, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Fire Extinguisher", :attack_damage=>2, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+#most powerful weapon
+Item.create(:name =>"Nerf Gun", :attack_damage=>100, :hp_modifier=>0, :in_backpack?=>false, :room_id=>nil)
+#####################
+Item.create(:name =>"A Baby Seal", :attack_damage=>5, :hp_modifier=>1, :in_backpack?=>false, :room_id=>nil)
+Item.create(:name =>"Charizard (First Edition) 'Wow!'", :attack_damage=>5, :hp_modifier=>5, :in_backpack?=>false, :room_id=>nil)
 
 
+# rooms
 Room.create({:x_coordiante => 1, :y_coordinate => 1, :north => FALSE, :south => TRUE, :east => FALSE, :west => FALSE, :description => "You are in your buddy Steve's office. It's a horrible mess"})
 Room.create({:x_coordiante => 2, :y_coordinate => 1, :north => FALSE, :south => TRUE, :east => FALSE, :west => FALSE, :description => "You are in your boss's office. It is covered in Japanese paraphernalia, despite him having no heritage from the region."})
 Room.create({:x_coordiante => 3, :y_coordinate => 1, :north => FALSE, :south => TRUE, :east => TRUE, :west => FALSE, :description => "You are in the foyer to your boss's receptionist's personal assistant's office.  You suddenly remember how ridiculous your former life was."})
