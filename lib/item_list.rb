5eb785911a6ecf26445bc99a1efd77153769f837
Brent Bailey <brentlbailey@gmail.com>
2015-05-20 12:59:16 -0700

    text to action functional for attack and take

--- lib/item_list.rb
+++ lib/item_list.rb
@@ -1,4 +1,4 @@
 
 class Item
-	Item.create(name: "wrench", usable?: true, weapon?: true, in_backpack?: false, room_id: 46, attack_damage: 30)
+	Item.create(name: "wrench", usable?: true, weapon?: true, in_backpack?: false, room_id: 51, attack_damage: 30)
 end
